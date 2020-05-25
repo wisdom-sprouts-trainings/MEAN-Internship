8f500318e109e97d38f484743b9c1c31dc9c06ee
Anuj bidkar <anujbidkar8@gmail.com>
2020-05-25 12:04:42 +0530

    updated

--- routes/product.js
+++ routes/product.js
@@ -10,22 +10,6 @@ const storage = multer.diskStorage({
     cb(null,file.originalname);
   }
 });
-// const fileFilter = (req, file, cb) => {
-//   // reject a file
-//   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//     cb(null, true);
-//   } else {
-//     cb(null, false);
-//   }
-// };
-
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 1024 * 1024 * 5
-//   },
-//   fileFilter: fileFilter
-// });
 
 var upload = multer({ storage: storage })
 const {
